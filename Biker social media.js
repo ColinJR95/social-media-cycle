9c570220a38d5819ef84cbcac95546f83a520214
ColinJR95 <javcolin1@gmail.com>
2020-05-13 23:04:09 -0700

    Add files via upload

---
+++ Biker social media.js
@@ -0,0 +1,45 @@
+var database = [
+	{
+		username: "mike",
+		password: "jones"
+	},
+	{
+		username: "mrjohnson",
+		password: "123"
+	}
+]; 
+
+var bikernewsfeed = [
+	{
+		username: "jr",
+		post: "I am ready for another ride"
+	},
+	{
+		username: "joe",
+		post: "heading to an 90 mile"
+	}
+
+];
+
+function isUserValid(username,password) {
+	for (var i = 0; i < database.length; i++) {
+		if(database[i].username === username && database[i].password === password) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function signIn(username, password) {
+	if (isUserValid(username,password)) {
+		console.log(newsfeed);
+	} else {
+		alert("sorry you entered the wrong info");
+	}
+}
+
+var userNamePromt = prompt("What\'s' your username?");
+var passwordPromt = prompt("What\'s your password?")
+
+signIn(userNamePromt, passwordPromt);
+
